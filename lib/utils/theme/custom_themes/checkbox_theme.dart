4affefe1894887c73161591711ca01ef20ac5fe8
PhucLeDio <dinhphucle3@gmail.com>
2024-03-02 15:41:57 +0700

    Feat: Light and Dark Theme added

---
+++ lib/utils/theme/custom_themes/checkbox_theme.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+/// Custom light dark theme
+class TCheckBoxTheme {
+  TCheckBoxTheme._();
+
+  // light text theme
+  static CheckboxThemeData lightCheckBoxTheme = CheckboxThemeData(
+    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
+    checkColor: MaterialStateProperty.resolveWith((states) {
+      if (states.contains(MaterialState.selected)) {
+        return Colors.white;
+      } else {
+        return Colors.black;
+      }
+    }),
+    fillColor: MaterialStateProperty.resolveWith((states) {
+      if (states.contains(MaterialState.selected)) {
+        return Colors.blue;
+      } else {
+        return Colors.transparent;
+      }
+    })
+  );
+
+  // dark text theme
+  static CheckboxThemeData darkCheckBoxTheme = CheckboxThemeData(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
+      checkColor: MaterialStateProperty.resolveWith((states) {
+        if (states.contains(MaterialState.selected)) {
+          return Colors.white;
+        } else {
+          return Colors.black;
+        }
+      }),
+      fillColor: MaterialStateProperty.resolveWith((states) {
+        if (states.contains(MaterialState.selected)) {
+          return Colors.blue;
+        } else {
+          return Colors.transparent;
+        }
+      })
+  );
+}
