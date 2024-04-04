b08737d04981be06bde062a956218b06ac964db7
PhucLeDio <dinhphucle3@gmail.com>
2024-04-04 19:19:58 +0700

    feat: Clip Path

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,10 +1,33 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_ecommerce/common/widgets/custom_shapes/curved_edges/curved_edges.dart';
+import 'package:flutter_ecommerce/utils/constants/colors.dart';
+
+import '../../../../common/widgets/custom_shapes/containers/circular_container.dart';
+import '../../../../common/widgets/custom_shapes/containers/primary_header_container.dart';
+import '../../../../common/widgets/custom_shapes/curved_edges/curved_edges_widget.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            TPrimaryHeaderContainer(
+              child: Container(),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
+
+
+
+
+
