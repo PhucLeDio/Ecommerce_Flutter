c96c51758b29e17c65061ec0c6fee772e5798eda
PhucLeDio <dinhphucle3@gmail.com>
2024-04-21 08:46:28 +0700

    feat: New logo & load image in setting

--- lib/common/widgets/list_tiles/user_profile_tile.dart
+++ lib/common/widgets/list_tiles/user_profile_tile.dart
@@ -19,7 +19,8 @@ class TUserProfileTile extends StatelessWidget {
   Widget build(BuildContext context) {
     final controller = UserController.instance;
     return ListTile(
-      leading: const TCircularImage(image: TImages.user, width: 50, height: 50, padding: 0),
+      // leading: const TCircularImage(image: TImages.user, width: 50, height: 50, padding: 0),
+      leading: Image.network(controller.user.value.profilePicture, width: 50, height: 50),
       title: Text(controller.user.value.fullName, style: Theme.of(context).textTheme.headlineSmall!.apply(color: TColors.white)),
       subtitle: Text(controller.user.value.email, style: Theme.of(context).textTheme.bodyMedium!.apply(color: TColors.white)),
       trailing: IconButton (onPressed: onPressed, icon: const Icon (Iconsax.edit, color: TColors.white)),
