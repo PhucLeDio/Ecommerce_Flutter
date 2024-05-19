a994113d4a1a402dc4a77551b7751c48073bde29
PhucLeDio <dinhphucle3@gmail.com>
2024-05-19 08:28:22 +0700

    feat: chatbot

---
+++ lib/features/shop/screens/assistant/chat_bot.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+import '../../../../../../utils/constants/colors.dart';
+
+class ViewChatBot extends StatefulWidget {
+  const ViewChatBot({super.key});
+
+  @override
+  State<ViewChatBot> createState() => _ViewChatBotState();
+}
+
+class _ViewChatBotState extends State<ViewChatBot> {
+  final controller = WebViewController()
+    ..setJavaScriptMode(JavaScriptMode.unrestricted)
+    ..loadRequest(Uri.parse("https://chatgpt.com/?oai-dm=1"));
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: BackButton(color: Colors.black,),
+        title: Text("ChatBot panel"),
+      ),
+
+      body: WebViewWidget(controller: controller,),
+    );
+  }
+}
